9fa9b936e0c70d85d1dffb5103421734c75709d5
ekwize <ulanovsega@gmail.com>
2024-03-25 14:09:22 +0300

    remove docker files and refastor gitignore

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:latest
-WORKDIR /app
-COPY requirements.txt requirements.txt
-RUN pip install --upgrade setuptools
-RUN pip install --no-cache-dir - r requirements.txt
-COPY . .
--- core/db/redis/redis.py
+++ core/db/redis/redis.py
@@ -3,6 +3,5 @@ from aioredis.client import Redis
 async def create_redis_pool(config):
     return await Redis(host = config.redis.redis_host,
                 port = config.redis.redis_port,
-                db = config.redis.redis_db,
-                password = config.redis.redis_password)
+                db = config.redis.redis_db)
 
